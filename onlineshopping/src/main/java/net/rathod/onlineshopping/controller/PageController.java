45128b0174d453d008fd9d402ebc7434c543d48d
Santosh Rathod <santoshrathod1996@gmail.com>
2020-03-17 21:08:32 +0530

    after update code

---
+++ onlineshopping/src/main/java/net/rathod/onlineshopping/controller/PageController.java
@@ -0,0 +1,22 @@
+package net.rathod.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = {"/","home","index"})
+		
+		public ModelAndView index() {
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "welcome to spring web MVC");
+		
+		return mv;
+		
+		
+	}
+	
+}
